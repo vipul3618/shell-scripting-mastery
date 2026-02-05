1eb8fc7e3451d438c435f5b253cfea5dbd0217d4
Dhanush <130854580+vipul3618@users.noreply.github.com>
2026-02-06 00:42:09 +0530

    Update

--- tasks/Variables/var.sh
+++ tasks/Variables/var.sh
@@ -2,5 +2,27 @@ Tasks:
 	1. Store your username in a variable and print it.
 	2. Assign two numbers to variables and print their sum.
 	3. Store a command output (date) into a variable.
-	4. Demonstrate the difference between $var and "${var}".
-	5. Create a variable holding a directory path and use it in a command.
+	4. Create a variable holding a directory path and use it in a command.
+
+#!/bin/bash
+
+username=$(whoami)
+echo -e "The username is: $username"
+
+echo "----------------------------"
+
+a=5
+b=4
+c=$((a+b))
+
+echo "The sum of no. $a or $b is: $c"
+
+echo "----------------------------"
+
+d=$(date)
+echo "Today is: $d"
+
+echo "----------------------------"
+
+myfile=/home/ubuntu/
+ls $myfile
