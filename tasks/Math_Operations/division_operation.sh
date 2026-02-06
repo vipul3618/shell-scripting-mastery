995c315686e82a8920bae875c3b5e1a64a132deb
Dhanush <130854580+vipul3618@users.noreply.github.com>
2026-02-07 00:09:47 +0530

    update

--- tasks/Math_Operations/division_operation.sh
+++ tasks/Math_Operations/division_operation.sh
@@ -5,3 +5,19 @@ read -p "Enter two numbers: " a b
 result=$(echo "scale=3; $a / $b" | bc)
 
 echo "Result: $result"
+
+echo "----------------------------------"
+
+
+echo "The no. 5 and 2"
+
+echo "scale=2; 5 / 2" | bc
+
+##scale=2 → number of digits after the decimal point
+echo "----------------------------------"
+
+echo "7 and 3"
+
+echo "7 / 3" | bc -l
+
+echo "scale=4; 7 / 3" | bc -l
