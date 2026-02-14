c0c40f6cc03da4be371b303d9d5bc9e7f4d3944c
Dhanush <130854580+vipul3618@users.noreply.github.com>
2026-02-14 18:40:22 +0530

    for loop

---
+++ tasks/loops/greet.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+names=("Alice" "Bob" "Charlie" "David")
+
+for name in "${names[@]}"
+do
+  echo "Hello, $name!"
+done
+
+
+# for name in Alice Bob Charlie David
+# do
+#   echo "Hello, $name!"
+# done
