cc04880e63de70ddb27fed49efa69fb0713bd1e2
Dhanush <130854580+vipul3618@users.noreply.github.com>
2026-02-17 23:55:05 +0530

    .

---
+++ tasks/arguments/arguments1.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "Script Name: $0"
+echo "Total Arguments: $#"
+echo "All Arguments: $@"
+
+echo "First Argument: $1"
+echo "Second Argument: $2"
+
+
+
+# ./arguments1.sh apple mango banana
+# bash arguments1.sh apple mango banana
